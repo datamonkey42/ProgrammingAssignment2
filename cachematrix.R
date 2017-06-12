5be48ec9862567f8930a849a928d5b36003a37bd
datamonkey42 <aaron.shaha@protonmail.com>
2017-06-11 20:39:23 -0600

    Added cache functions
    updated the code for programming assignment 2, check to see if equality in cacheSolve, if so, compute.  if not return cached value

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,30 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## makes a cache of a matrix (x), use set and get to returns values
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
 
+  set <- function(y){
+    x <<- y
+    m <<- NULL
+  }
+  
+  get <- function() x
 }
 
 
 ## Write a short comment describing this function
-
+## check the cache, the pull a copy or force a compute
+##if they are identical we can then compute solve( data ) 
+##otherwise we just return the value as we have it cached
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+ m <- x$get()
+ if ( identical(x,m) ){
+   data <- x$get()
+   m <- solve(data)
+ }
+ else { return(m) }
+ m
 }
